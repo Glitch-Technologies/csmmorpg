ae34edfca7a55c85f12fa6a11cef8062aaa66265
greenturtle537 <greenturtle537@mail.com>
2023-07-09 20:51:05 +0000

    figured out images

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-const canvas = document.querySelector(".myCanvas");
-const width = (canvas.width = window.innerWidth);
-const height = (canvas.height = window.innerHeight);
-const ctx = canvas.getContext("2d");
+function draw() {
+    const canvas = document.getElementById("canvas");
+    if (canvas.getContext) {
+    
+        const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "rgb(0, 0, 0)";
-ctx.fillRect(0, 0, width, height);
+        ctx.fillStyle = "rgb(200, 0, 0)";
+        ctx.fillRect(10, 10, 50, 50);
+
+        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+        ctx.fillRect(30, 30, 50, 50);
+
+        const img = new Image(100,100);
+        console.log("0")
+        img.onload = () => {
+            ctx.drawImage(img, 0, 0);
+        }
+        img.src = "myimage.png";
+
+    }
+  }
+  draw();
